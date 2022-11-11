e2b8353457bd818e9b41eb5c636daea8c66c1986
Moin <me@moin.im>
2022-11-11 19:47:45 +0500

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 // import "@botui/react/default-theme"
 
@@ -6,7 +7,24 @@ import '../styles/homebot.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-587LLC14KB'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-587LLC14KB');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
